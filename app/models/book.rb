4ede1d43316464f0986514e58a4a35740ae3e93c
Vic Lam <lam.vic.k@gmail.com>
2017-01-10 16:11:29 -0500

    Add average ratings to show pages

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,10 @@ class Book < ApplicationRecord
   has_many :reviews, as: :reviewable
   validates :title, presence: true, uniqueness: true
   validates :author, presence: true
+
+  def average_rating
+    ratings = reviews.collect { |review| review.rating }
+    (ratings.inject{ |sum, el| sum + el }.to_f / ratings.size).round(2)
+  end
+
 end
