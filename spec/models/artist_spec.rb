02d3bb55eb105e8a868394773a0f2f031d32dd42
Andrew Cohn <andrewcohn0@gmail.com>
2017-01-11 17:38:41 -0500

    fixed artist spec tests

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -2,19 +2,19 @@ require_relative '../rails_helper'
 
 describe Artist do
 
-  let(:artist) { Artist.new(name: 'Mango')}
+  let(:artist) { Artist.new(title: 'Mango')}
   let(:invalid_artist) { Artist.new }
 
-  it 'has a name attribute' do
-    expect(artist).to have_attributes(name: 'Mango')
+  it 'has a title attribute' do
+    expect(artist).to have_attributes(title: 'Mango')
   end
 
-  it 'is invalid without a name' do
+  it 'is invalid without a title' do
     invalid_artist.valid?
-    expect(invalid_artist.errors.full_messages).to include("Name can't be blank")
+    expect(invalid_artist.errors.full_messages).to include("Title can't be blank")
   end
 
-  it 'is valid with a name' do
+  it 'is valid with a title' do
     expect(artist.errors).to be_empty
   end
 
