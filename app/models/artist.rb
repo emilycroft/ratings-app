495b3d4a5cda78fefda15155c0cbebf824ae6535
Vic Lam <lam.vic.k@gmail.com>
2017-01-13 10:06:57 -0500

    Split Reviewables module into Instance and Class methods

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,6 @@
 class Artist < ApplicationRecord
-  include Reviewables
+  include Reviewables::InstanceMethods
+  extend Reviewables::ClassMethods
   has_many :reviews, as: :reviewable
   validates :title, presence: true, uniqueness: true
 
--- app/models/reviewables.rb
+++ app/models/reviewables.rb
@@ -1,12 +1,26 @@
 module Reviewables
 
-  def average_rating
-    if !reviews.empty?
-      ratings = reviews.collect { |review| review.rating }
-      (ratings.inject{ |sum, el| sum + el }.to_f / ratings.size).round(2)
-    else
-      "No reviews"
+  module InstanceMethods
+
+    def average_rating
+      if !reviews.empty?
+        ratings = reviews.collect { |review| review.rating }
+        (ratings.inject{ |sum, el| sum + el }.to_f / ratings.size).round(2)
+      else
+        "No reviews"
+      end
     end
+
   end
+
+  module ClassMethods
+
+    def top_5
+      puts "Yay!"
+    end
+
+  end
+
+
 
 end
